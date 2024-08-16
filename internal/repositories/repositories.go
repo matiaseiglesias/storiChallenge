5c095cbb106021971195ccbe00f2319a41801ec6
matiaseiglesias <matiaseiglesias@yahoo.com>
2024-08-16 18:08:15 -0300

    adding respository layer to interact with db

--- internal/repositories/repositories.go
+++ internal/repositories/repositories.go
@@ -3,7 +3,7 @@ package repositories
 import "github.com/matiaseiglesias/storiChallenge/internal/database"
 
 type Repositories struct {
-	TransactionsSummary *TransactionsSummaryRepository
+	TransactionsSummary TransactionsSummaryRepository
 }
 
 func CreateRepositories(db *database.DataBase) *Repositories {
--- internal/repositories/transactions_summary.go
+++ internal/repositories/transactions_summary.go
@@ -2,23 +2,41 @@ package repositories
 
 import (
 	"github.com/matiaseiglesias/storiChallenge/internal/database"
+	"github.com/matiaseiglesias/storiChallenge/internal/dto"
 	"github.com/matiaseiglesias/storiChallenge/internal/models"
 )
 
-type TransactionsSummaryRepository struct {
+type TransactionsSummaryRepository interface {
+	SaveTransactionSummary(summary *models.Summary)
+}
+
+type TransactionsSummaryRepositoryImpl struct {
 	db *database.DataBase
 }
 
-func CreateTransactionsSummaryRepository(db *database.DataBase) *TransactionsSummaryRepository {
+func CreateTransactionsSummaryRepository(db *database.DataBase) *TransactionsSummaryRepositoryImpl {
 
-	return &TransactionsSummaryRepository{
+	return &TransactionsSummaryRepositoryImpl{
 		db: db,
 	}
 }
 
-func (r *TransactionsSummaryRepository) GetTransactionSummary(filename string) {
+func (r *TransactionsSummaryRepositoryImpl) GetTransactionSummary(filename string) {
 }
 
-func (r *TransactionsSummaryRepository) SaveTransactionSummary(summary models.Transaction) {
-
+func (r *TransactionsSummaryRepositoryImpl) SaveTransactionSummary(summary *models.Summary) {
+	transactionsCount := []dto.TransactionsCountDto{}
+	for _, v := range summary.TransactionsCount {
+		transactionsCount = append(transactionsCount, dto.TransactionsCountDto{
+			Month:  v.Month,
+			Amount: v.Amount,
+		})
+	}
+	summary_ := &dto.SummaryDto{
+		TotalBalance:      summary.TotalBalance.String(),
+		AverageCredit:     summary.AverageCredit.String(),
+		AverageDebit:      summary.AverageDebit.String(),
+		TransactionsCount: transactionsCount,
+	}
+	r.db.Db.Create(summary_)
 }
